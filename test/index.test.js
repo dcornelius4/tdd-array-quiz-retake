2d9f8c069d7be893f4a4c958a57cc97d490505ed
Dave Cornelius <d_cornelius94@yahoo.com>
2019-04-12 15:24:01 -0700

    got filter to pass!!!!!

--- test/index.test.js
+++ test/index.test.js
@@ -2,8 +2,8 @@ const newArray = require('../lib/index');
 
 describe('New array', () => {
   it('filters out numbers less than 5', () => {
-    const oldArray = [1, 3, 5, 7, 2];
-    const newArray = [5, 7];
-    expect(oldArray).toEqual(newArray);
+    const oldArray = newArray[1, 3, 5, 7, 2];
+    const updatedArray = newArray[5, 7];
+    expect(oldArray).toEqual(updatedArray);
   });
 });
